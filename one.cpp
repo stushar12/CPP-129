d3f9de7c1753ecf374845743977499e48bf7b9e3
Tushar Saxena <stushar345@gmail.com>
2021-02-17 23:26:13 +0530

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -1,31 +1,42 @@
-int minSwap(int *arr, int n, int k) 
-{ 
-	
-	int count = 0; 
-	for (int i = 0; i < n; ++i) 
-		if (arr[i] <= k) 
-			++count; 
-	
-	int bad = 0; 
-	for (int i = 0; i < count; ++i) 
-		if (arr[i] > k) 
-			++bad; 
-	
-	int ans = bad; 
-	for (int i = 0, j = count; j < n; ++i, ++j) { 
-		
-		// Decrement count of previous window 
-		if (arr[i] > k) 
-			--bad; 
-		
-		// Increment count of current window 
-		if (arr[j] > k) 
-			++bad; 
-		
-		// Update ans if count of 'bad' 
-		// is less in current window 
-		ans = min(ans, bad); 
-	} 
-	return ans; 
-} 
+#include <bits/stdc++.h>
+using namespace std;
 
+int minSwap(int arr[], int n, int k)
+{
+
+	int good = 0;
+	for (int i = 0; i < n; ++i)
+		if (arr[i] <= k)
+			++good;
+
+	int bad = 0;											
+	for (int i = 0; i < good; ++i)										//no of bad in window of size of good
+		if (arr[i] > k)
+			++bad;
+
+	int ans = bad;
+
+	for (int i = 0, j = good; j < n; ++i, ++j)
+	{
+
+		// Decrement count of previous window
+		if (arr[i] > k)
+			--bad;
+
+		// Increment count of current window
+		if (arr[j] > k)
+			++bad;
+
+		ans = min(ans, bad);
+	}
+	return ans;
+}
+
+int main()
+{
+	int arr1[] = {2, 7, 9, 5, 8, 7, 4};
+	int n = (sizeof(arr1) / sizeof(arr1[0]));
+	int k=6;
+	cout<<minSwap(arr1,n,k);
+
+}
